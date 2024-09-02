a83a32107c3495a2a7e183a8ee79de07532a2891
neet-007 <moayedeffect@gmail.com>
2024-09-03 02:56:01 +0300

    added links to creditinals

--- src/components/Credintails.tsx
+++ src/components/Credintails.tsx
@@ -5,32 +5,35 @@ import { useFadeIn } from "../hooks/fadeIn";
 import { PatchCheck } from "./PatchCheck";
 import { CREDINTAILS } from "../constants";
 
-const Credential: FC<ComponentProps<"div"> & { title: string, img: string, theme: "light" | "dark" }> = ({
-  title, img, theme
+const Credential: FC<ComponentProps<"div"> & {
+  title: string, img: string,
+  theme: "light" | "dark", link: string
+}> = ({
+  title, img, link, theme
 }) => {
 
-  return (
-    <div className={`flex f-d-column gap-1 w-100 p-4 b-radius-1 p-4 
+    return (
+      <div className={`flex f-d-column gap-1 w-100 p-4 b-radius-1 p-4 
         ${theme === "light" ?
-        "bg-light-gray" : "bg-dark-gray"}
+          "bg-light-gray" : "bg-dark-gray"}
         fade-in`}
-      data-appear="false">
-      <div className="f-size-sm cap">
-        {title}
+        data-appear="false">
+        <div className="f-size-sm cap">
+          {title}
+        </div>
+        <div>
+          <img src={img} alt="" className="b-1-solid-black b-radius-75" />
+        </div>
+        <a href={link} target="_blank" className="p-y-2 ">
+          <Button theme={theme} className="animate-bg-color flex align-items-center gap-1">
+            <PatchCheck iconHeight="20" iconWidth="20" theme={theme} util />
+            <p>verify</p>
+          </Button>
+        </a>
       </div>
-      <div>
-        <img src={img} alt="" className="b-1-solid-black b-radius-75" />
-      </div>
-      <a href="" className="p-y-2 ">
-        <Button theme={theme} className="animate-bg-color flex align-items-center gap-1">
-          <PatchCheck iconHeight="20" iconWidth="20" theme={theme} util />
-          <p>verify</p>
-        </Button>
-      </a>
-    </div>
 
-  )
-}
+    )
+  }
 
 export const Credintails: FC<ComponentProps<"div">> = () => {
   const { theme } = useThemeContext();
@@ -44,7 +47,7 @@ export const Credintails: FC<ComponentProps<"div">> = () => {
       <div className="grid g-t-columns-4-1fr gap-2 p-2 place-items-center" ref={containerRef}>
         {CREDINTAILS.map((v, i) => (
           <Credential key={`project-${v.title}-${i}`} title={v.title}
-            img={v.img} theme={theme} />
+            img={v.img} theme={theme} link={v.link} />
         ))}
       </div>
     </div>
--- src/constants.ts
+++ src/constants.ts
@@ -9,11 +9,11 @@ export const PROJECTS = [
 ]
 
 export const CREDINTAILS = [
-          { title: "Meta front end specialization", img: "/meta_front_end.jpg" },
-          { title: "Meta back end specialization", img: "/meta_back_end.jpg" },
-          { title: "Advanced Django specialization", img: "/codio_django.jpg" },
-          { title: "US Diego algorithmic toolbox", img: "/algorithms_toolbox.jpg" },
-          { title: "US Diego data structures", img: "/data_structures.jpg" },
-          { title: "US Diego algorithms on strings", img: "/algorithms_on_strings.jpg" },
-          { title: "US Diego algorithms on graphs", img: "/algorithms_on_graphs.jpg" },
+{ title: "Meta front end specialization", img: "/meta_front_end.jpg", link:"https://www.coursera.org/account/accomplishments/professional-cert/P2QJPFBRAZ9M" },
+{ title: "Meta back end specialization", img: "/meta_back_end.jpg", link:"https://www.coursera.org/account/accomplishments/professional-cert/HPTF5QL4WHBG" },
+{ title: "Advanced Django specialization", img: "/codio_django.jpg", link:"https://www.coursera.org/account/accomplishments/specialization/CKTX4XWT5TQB" },
+{ title: "US Diego algorithmic toolbox", img: "/algorithms_toolbox.jpg", link:"https://www.coursera.org/account/accomplishments/verify/8THEQPAP6HGU" },
+{ title: "US Diego data structures", img: "/data_structures.jpg", link:"https://www.coursera.org/account/accomplishments/verify/4KTLYQ6EPCZP" },
+{ title: "US Diego algorithms on strings", img: "/algorithms_on_strings.jpg", link:"https://www.coursera.org/account/accomplishments/verify/89X86ACMVK82" },
+{ title: "US Diego algorithms on graphs", img: "/algorithms_on_graphs.jpg", link:"https://www.coursera.org/account/accomplishments/verify/HG4BX7N9HBFL" },
         ]
