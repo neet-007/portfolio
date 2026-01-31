62f2ea32a9fc402c15cd847ec4599fe5bc60dd66
neet-007 <moayedeffect@gmail.com>
2026-01-31 17:38:46 +0300

    update projects

--- src/constants.ts
+++ src/constants.ts
@@ -1,39 +1,43 @@
 export const PROJECTS = [
-	{
-		title: "glox", about: `an interpreter for lox language, featuring a full tree-walk interpreter
+    {
+        title: "glox", about: `an interpreter for lox language, featuring a full tree-walk interpreter
 		  with modules for scanning, parsing, resolving, and evaluation, its written in go and only uses the standard library`, img: "public/glox_still.png",
-		gif: "public/glox_gif.gif", github: "https://github.com/neet-007/glox", link: ""
-	},
-	{
-		title: "text editor in c", about: `is a lightweight text editor written in C, based on the Kilo editor.
+        gif: "public/glox_gif.gif", github: "https://github.com/neet-007/glox", link: ""
+    },
+    {
+        title: "text editor in c", about: `is a lightweight text editor written in C, based on the Kilo editor.
 		It includes features such as auto-indentation, basic clipboard support, and an INI-based configuration system.
 		it is built using only core libraries, and supports editing files, line numbers, and syntax highlighting.`,
-		img: "/text_editor_c_still.png", gif: "/text_editor_c_gif.gif", github: "https://github.com/neet-007/text_editor_c", link: ""
-	},
-	{
-		title: "suffix tree visualizer", about: `react app made with typescript, takes text as user input
+        img: "/text_editor_c_still.png", gif: "/text_editor_c_gif.gif", github: "https://github.com/neet-007/text_editor_c", link: ""
+    },
+    {
+        title: "suffix tree visualizer", about: `react app made with typescript, takes text as user input
 		  				    and visualize the steps of constructing suffix tree, spped and alphabet sorting are adjustable`, img: "/suffix_tree_still.png", gif: "/suffix_tree_gif.gif",
-		github: "https://github.com/neet-007/SuffixTree", link: "https://neet-007.github.io/SuffixTree/"
-	},
-	{
-		title: "sorting algorithms visualizer", about: `It's an app that compares sorting algorithms against each other or allows you to view them individually. Speed and array elements are adjustable, made with React and TypeScript`, img: "/sorting_algorithms_still.png", gif: "/sorting_algorithms_gif.gif",
-		github: "https://github.com/neet-007/Sorting-Algorithms-test", link: "https://neet-007.github.io/Sorting-Algorithms-test/"
-	},
-	{
-		title: "twitter clone", about: `A Twitter clone made with Django, React, and TypeScript.
+        github: "https://github.com/neet-007/SuffixTree", link: "https://neet-007.github.io/SuffixTree/"
+    },
+    {
+        title: "sorting algorithms visualizer", about: `It's an app that compares sorting algorithms against each other or allows you to view them individually. Speed and array elements are adjustable, made with React and TypeScript`, img: "/sorting_algorithms_still.png", gif: "/sorting_algorithms_gif.gif",
+        github: "https://github.com/neet-007/Sorting-Algorithms-test", link: "https://neet-007.github.io/Sorting-Algorithms-test/"
+    },
+    {
+        title: "twitter clone", about: `A Twitter clone made with Django, React, and TypeScript.
 		  Users can create accounts, get email verification, and then log in.
 		  User actions include tweeting, liking tweets, bookmarking, following/unfollowing,
 		  searching, and creating communities`, img: "/twitter_clone_still.png", gif: "/twitter_clone_gif.gif",
-		github: "https://github.com/neet-007/Twiiter-V2-typescript", link: ""
-	},
+        github: "https://github.com/neet-007/Twiiter-V2-typescript", link: ""
+    },
+    {
+        title: "charts app", about: "A simple charts app made with React and TypeScript. It allows you to create charts, save them, and share them with others.", img: "/charts_app_still.png", gif: "/charts_app_gif.gif",
+        github: "https://github.com/neet-007/chartsApp", link: "https://neet-007.github.io/chartsApp/"
+    }
 ]
 
 export const CREDINTAILS = [
-	{ title: "Meta front end specialization", img: "/meta_front_end.jpg", link: "https://www.coursera.org/account/accomplishments/professional-cert/P2QJPFBRAZ9M" },
-	{ title: "Meta back end specialization", img: "/meta_back_end.jpg", link: "https://www.coursera.org/account/accomplishments/professional-cert/HPTF5QL4WHBG" },
-	{ title: "Advanced Django specialization", img: "/codio_django.jpg", link: "https://www.coursera.org/account/accomplishments/specialization/CKTX4XWT5TQB" },
-	{ title: "US Diego algorithmic toolbox", img: "/algorithms_toolbox.jpg", link: "https://www.coursera.org/account/accomplishments/verify/8THEQPAP6HGU" },
-	{ title: "US Diego data structures", img: "/data_structures.jpg", link: "https://www.coursera.org/account/accomplishments/verify/4KTLYQ6EPCZP" },
-	{ title: "US Diego algorithms on strings", img: "/algorithms_on_strings.jpg", link: "https://www.coursera.org/account/accomplishments/verify/89X86ACMVK82" },
-	{ title: "US Diego algorithms on graphs", img: "/algorithms_on_graphs.jpg", link: "https://www.coursera.org/account/accomplishments/verify/HG4BX7N9HBFL" },
+    { title: "Meta front end specialization", img: "/meta_front_end.jpg", link: "https://www.coursera.org/account/accomplishments/professional-cert/P2QJPFBRAZ9M" },
+    { title: "Meta back end specialization", img: "/meta_back_end.jpg", link: "https://www.coursera.org/account/accomplishments/professional-cert/HPTF5QL4WHBG" },
+    { title: "Advanced Django specialization", img: "/codio_django.jpg", link: "https://www.coursera.org/account/accomplishments/specialization/CKTX4XWT5TQB" },
+    { title: "US Diego algorithmic toolbox", img: "/algorithms_toolbox.jpg", link: "https://www.coursera.org/account/accomplishments/verify/8THEQPAP6HGU" },
+    { title: "US Diego data structures", img: "/data_structures.jpg", link: "https://www.coursera.org/account/accomplishments/verify/4KTLYQ6EPCZP" },
+    { title: "US Diego algorithms on strings", img: "/algorithms_on_strings.jpg", link: "https://www.coursera.org/account/accomplishments/verify/89X86ACMVK82" },
+    { title: "US Diego algorithms on graphs", img: "/algorithms_on_graphs.jpg", link: "https://www.coursera.org/account/accomplishments/verify/HG4BX7N9HBFL" },
 ]
